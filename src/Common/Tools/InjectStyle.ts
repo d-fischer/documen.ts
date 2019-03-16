d5785d17d5b6da1e623156e9e32a6122d2897161
Daniel Fischer <fischer.daniel94@gmail.com>
2019-03-16 20:11:25 +0100

    fix new jss types

--- src/Common/Tools/InjectStyle.ts
+++ src/Common/Tools/InjectStyle.ts
@@ -1,7 +1,7 @@
 import injectSheet, { StyleCreator, Styles, WithSheet as OrigWithSheet } from 'react-jss';
 import { Theme } from '../Theme';
 
-type WithSheet<S extends string | Styles | StyleCreator> = OrigWithSheet<S, Theme>;
+type WithSheet<S extends string | Styles | StyleCreator<keyof S>> = OrigWithSheet<S, Theme>;
 
 function createStyles<T extends string>(styles: Styles<T> | StyleCreator<T, Theme>): Styles<T> | StyleCreator<T, Theme> {
 	return styles;
