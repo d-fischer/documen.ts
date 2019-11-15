0f342aad595af7f97cb13be18db73b2ea3016bbe
Daniel Fischer <daniel@d-fischer.dev>
2019-11-15 20:56:31 +0100

    fix double menu on index

--- src/Common/Pages/MonoIndexPage.tsx
+++ src/Common/Pages/MonoIndexPage.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import PageHeader from '../Containers/PageHeader';
 import PageContent from '../Containers/PageContent';
-import MonoMenu from '../Components/MonoMenu';
 
 const MonoIndexPage: React.FunctionComponent = () => (
 	<>
-		<MonoMenu/>
 		<PageHeader>
 			<h1>MONO</h1>
 		</PageHeader>
