4c841a05501989bd608c66414c7f196f403bd6ab
Daniel Fischer <daniel@d-fischer.dev>
2019-11-16 13:21:23 +0100

    make linter happy

--- src/Common/Tools/TypeTools.ts
+++ src/Common/Tools/TypeTools.ts
@@ -1,2 +1,3 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export declare type HookClassKey<T> = T extends (props: any) => Record<infer S, string> ? S : never;
 export declare type HookClassesOverride<HookType> = Partial<Record<HookClassKey<HookType>, string>>;
