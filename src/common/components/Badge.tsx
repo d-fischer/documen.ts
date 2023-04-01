185bc5790f0ffa7b73f3ad5f284af026a1bfd4e8
Daniel Fischer <daniel@d-fischer.dev>
2023-04-01 21:43:33 +0200

    open async explainer link in new tab

--- src/common/components/Badge.tsx
+++ src/common/components/Badge.tsx
@@ -1,31 +1,34 @@
+import type { DefaultTheme } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import classNames from 'classnames';
 import React from 'react';
-import type { DefaultTheme} from '@material-ui/styles';
-import { makeStyles } from '@material-ui/styles';
 
 interface BadgeStyleProps {
 	hasLink: boolean;
 }
 
-const useStyles = makeStyles<DefaultTheme, BadgeStyleProps>(theme => ({
-	root: {
-		display: 'inline-block',
-		color: theme.colors.background.default,
-		backgroundColor: theme.colors.text,
-		marginLeft: '1em',
-		padding: '2px 5px',
-		borderRadius: 5,
-		fontSize: 12,
-		lineHeight: 'normal',
-		textDecoration: 'none',
-		cursor: ({ hasLink }) => hasLink ? 'pointer' : 'default'
-	},
-	rootSmall: {
-		padding: '1px 3px',
-		fontSize: 10,
-		marginLeft: '.5em'
-	}
-}), { name: 'Badge' });
+const useStyles = makeStyles<DefaultTheme, BadgeStyleProps>(
+	theme => ({
+		root: {
+			display: 'inline-block',
+			color: theme.colors.background.default,
+			backgroundColor: theme.colors.text,
+			marginLeft: '1em',
+			padding: '2px 5px',
+			borderRadius: 5,
+			fontSize: 12,
+			lineHeight: 'normal',
+			textDecoration: 'none',
+			cursor: ({ hasLink }) => (hasLink ? 'pointer' : 'default')
+		},
+		rootSmall: {
+			padding: '1px 3px',
+			fontSize: 10,
+			marginLeft: '.5em'
+		}
+	}),
+	{ name: 'Badge' }
+);
 
 interface BadgeProps {
 	small?: boolean;
@@ -37,9 +40,23 @@ interface BadgeProps {
 const Badge: React.FC<React.PropsWithChildren<BadgeProps>> = ({ small, title, href, className, children }) => {
 	const hasLink = !!href;
 	const classes = useStyles({ hasLink });
-	const Component = hasLink ? 'a' : 'span';
+	if (hasLink) {
+		return (
+			<a
+				href={href}
+				target="_blank"
+				title={title}
+				className={classNames(classes.root, { [classes.rootSmall]: small }, className)}
+				rel="noreferrer"
+			>
+				{children}
+			</a>
+		);
+	}
 	return (
-		<Component href={href} title={title} className={classNames(classes.root, { [classes.rootSmall]: small }, className)}>{children}</Component>
+		<span title={title} className={classNames(classes.root, { [classes.rootSmall]: small }, className)}>
+			{children}
+		</span>
 	);
 };
 
